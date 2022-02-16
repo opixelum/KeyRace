016a5559c14aceec5981bc9c429b0d8d2aabcc93
Opixelum <12w0zvkgazd3@opayq.net>
2022-02-16 12:38:54 +0100

    Added background

--- test/script/main.js
+++ test/script/main.js
@@ -7,7 +7,7 @@ window.onload = function() {
         helmet.src = "assets/helmets/" + helmetName
     })
 
-    const helmet = new Image();
+    const helmet = new Image()
     const helmetNum = helmetDropdown.value
     const helmetName = "helmet" + helmetNum + ".png"
     helmet.src = "assets/helmets/" + helmetName
@@ -25,7 +25,7 @@ window.onload = function() {
         vest.src = "assets/vests/" + vestName 
     })
 
-    const vest = new Image();
+    const vest = new Image()
     const vestNum = vestDropdown.value
     const vestName = "vest" + vestNum + ".png"
     vest.src = "assets/vests/" + vestName 
@@ -35,12 +35,27 @@ window.onload = function() {
     }
 
 
+    // Background color
+    const bgColorDropdown = document.querySelector("#bg-color-dropdown")
+    let bgColor = "#000000"
+
+    bgColorDropdown.addEventListener("change", function() {
+        bgColor = '#' + bgColorDropdown.value
+        buildAvatar()
+    })
+
     // Add all assets to the avatar
     function buildAvatar() {
         const avatar = document.querySelector("#avatar")
         const context = avatar.getContext("2d")
         avatar.width = 300
         avatar.height = 300
+
+        // Draw rectangle as background
+        context.beginPath()
+        context.rect(0, 0, 300, 300)
+        context.fillStyle = bgColor
+        context.fill()
 
         context.drawImage(vest, 0, 0)
         context.drawImage(helmet, 0, 0)
