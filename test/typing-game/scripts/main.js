3c6e71d5d18e26a36db5fa23499cbe36de942bd0
Opixelum <12w0zvkgazd3@opayq.net>
2022-02-18 20:12:42 +0100

    Fixed untracked keys

--- test/typing-game/scripts/main.js
+++ test/typing-game/scripts/main.js
@@ -1,10 +1,13 @@
+// Used for excluding non-letter keys
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 const typingDiv = document.querySelector("#typing")
 
 // Text to type
 const text = "This is a test. Try to type as fast as possible."
 
 // Split the whole text by each character
-// Then creat a span for each of those
+// Then create a span for each of those
 const characters = text.split('').map((char) => {
     const span = document.createElement("span")
     span.innerText = char
@@ -25,20 +28,23 @@ let endTime = null
 
 // Listen to player's keyboard
 const keyListener = document.addEventListener("keydown", ({key}) => {
+    // Start stopwatch
     if (!startTime) {
         startTime = new Date()
     }
     
+    // Key check
     if (key === cursorCharacter.innerText) {
         // If right key was typed
         cursorCharacter.classList.remove("cursor")
         cursorCharacter.classList.add("done")
         cursorCharacter = characters[++cursorIndex]
-    } else {
-        // If wrong key was typed
+    } else if (letters.includes(key)) {
+        // If wrong key (excluding non-letter keys) was typed
         cursorCharacter.classList.add("wrong")
     }
 
+    // Compute all stats
     if (cursorIndex >= characters.length) {
         endTime = new Date()
 
@@ -60,10 +66,13 @@ const keyListener = document.addEventListener("keydown", ({key}) => {
         // Display stats
         document.querySelector("#stats").innerText = `WPM: ${wpm}`
 
-        // Prevent next lines to be executed after game done
+        // Prevent next lines to be executed when game is done
         document.removeEventListener("keydown", keyListener)
         return
     }
 
+    // If right key was typed, move the cursor to the next character
+    // Instruction outside "Key check" block in order to prevent errors when 
+    // game is done.
     cursorCharacter.classList.add("cursor")
 })
