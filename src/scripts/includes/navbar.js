470380a4210135aa756b5501f8d37d2355d93055
Opixelum <12w0zvkgazd3@opayq.net>
2022-02-20 16:29:13 +0100

    Added all html elements

--- src/scripts/includes/navbar.js
+++ src/scripts/includes/navbar.js
@@ -17,23 +17,49 @@
  */
 
 
+// Create "Navbar" class, inherited from "HTMLElement" class with "extends"
 class Navbar extends HTMLElement {
+
+    // Constructor method for initiate objects automatically
     constructor() {
-        super()
+        super() // Call "HTMLElement"'s constructor
+
+        // Our Navbar is a "shadow" element that will be attached to the DOM
+        // of our website's pages.
+        // The 'mode: "open"' makes the shadow element visible globally to all
+        // of our JavaScript code.
         this.shadow = this.attachShadow({mode: "open"})
     }
 
+    // Method called when "nav-bar" is inserted in the DOM
     connectedCallback() {
         this.render()
     }
 
     render() {
         this.shadow.innerHTML = `
-            <nav>
+            <div id="navbar">
               <img alt="KeyRace logo" width="50px" src="./src/images/logo.png">
-            </nav>
+
+              <input type="search" id="search-field" name="search-field"
+              placeholder="Search a player">
+
+              <nav>
+                <ul>
+                  <li><button id="login-btn">Log in</button></li>
+                  <li><button id="signin-btn">Sign in</button></li>
+                  <li><button id="leaderboard-btn">Leaderboard</button></li>
+                  <li><button id="settings-btn">Settings</button></li>
+                </ul>
+              </nav>
+
+              <a href="./src/php/contact.php">Support</a>
+
+              <small>© KeyRace 2022</small>
+            </div>
         `
     }
 }
 
+// Make our elememt available for the HTML
 customElements.define("nav-bar", Navbar)
