2a5bf00e364f63832e402615f1d58a31789cbb65
Opixelum <12w0zvkgazd3@opayq.net>
2022-03-06 18:15:47 +0100

    Fixed first version of light/dark mode

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -1,42 +1,38 @@
 //Dark mode / Light mode
 
+// 0 = dark mode, 1 = light mode
 let mode = 0
+
 const navbar = document.querySelector("#navbar")
 const main = document.querySelector("#main")
-const sign_in_btn = document.querySelector("#sign-in-btn")
-const log_in_btn = document.querySelector("#log-in-btn")
-const leaderboard_btn = document.querySelector("#leaderboard-btn")
-const settings_btn = document.querySelector("#settings-btn")
-const start_btn = document.querySelector("#start-btn")
 const search_field = document.querySelector("#search-field")
 const body = document.querySelector("body")
 const modeBtn = document.querySelector("#mode-btn")
-const btnList = {
-    sign_in_btn,
-    log_in_btn,
-    leaderboard_btn,
-    settings_btn,
-    start_btn,
-    search_field,
-    modeBtn
-}
+
+// Select all buttons on website
+const allBtns = document.getElementsByTagName("button")
 
 modeBtn.addEventListener("click", function() {
     if (mode == 0) {
+        // If current mode is dark, change to light mode
         navbar.classList.add("light")
         main.classList.add("light")
-        body.style.backgroundColor = "#ffffff"
-        for (let i = 0; i < btnList.length; i++) {
-            btnList[i].style.color = "#ffffff"
+        body.style.backgroundColor = "white"
+
+        // Change all buttons font color to white
+        for (let i = 0; i < allBtns.length; i++) {
+            allBtns[i].style.color = "white"
         }
         mode = 1
-    }
-    else {
+    } else {
+        // If current mode is light, change to dark mode
         navbar.classList.remove("light")
         main.classList.remove("light")
-        body.style.backgroundColor = "#000000";
-        for (let i = 0; i < btnList.length; i++) {
-            btnList[i].style.color = "#000000"
+        body.style.backgroundColor = "black";
+
+        // Change all buttons font color to black 
+        for (let i = 0; i < allBtns.length; i++) {
+            allBtns[i].style.color = "black"
         }
         mode = 0
     }
