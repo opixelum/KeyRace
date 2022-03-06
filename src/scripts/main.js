17ce174deb7446d43bd270caa2509e2cd58b67cc
Romain <romain.nerot@outlook.fr>
2022-03-06 14:46:11 +0100

    Return to dark mode

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,23 +27,52 @@ startBtn.addEventListener("click", function() {
     window.location.href = "./signin.php"
 })
 
+let mode = 0
 //Dark mode / Light mode
 const modeBtn = document.querySelector("#settings-btn")
 modeBtn.addEventListener("click", function() {
-    const darkNavbar = document.querySelector(".rgb-shadow-dark")
-    darkNavbar.className = "navbar col-2 p-0 me-2 rounded rgb-shadow-light"
-    const darkMain = document.querySelector(".rgb-shadow-dark")
-    darkMain.className = "main col ms-2 rounded rgb-shadow-light"
-    darkBody = document.querySelector("body")
-    darkBody.style.backgroundColor = "#ffffff";
-    const sign_in_btn = document.querySelector("#sign-in-btn")
-    sign_in_btn.style.color = "#ffffff"
-    const log_in_btn = document.querySelector("#log-in-btn")
-    log_in_btn.style.color = "#ffffff"
-    const leaderboard_btn = document.querySelector("#leaderboard-btn")
-    leaderboard_btn.style.color = "#ffffff"
-    const settings_btn = document.querySelector("#settings-btn")
-    settings_btn.style.color = "#ffffff"
-    const start_btn = document.querySelector("#start-btn")
-    start_btn.style.color = "#ffffff"
+    if (mode == 0)
+    {
+        const darkNavbar = document.querySelector(".rgb-shadow-dark")
+        darkNavbar.className = "navbar col-2 p-0 me-2 rounded rgb-shadow-light"
+        const darkMain = document.querySelector(".rgb-shadow-dark")
+        darkMain.className = "main col ms-2 rounded rgb-shadow-light"
+        darkBody = document.querySelector("body")
+        darkBody.style.backgroundColor = "#ffffff";
+        const sign_in_btn = document.querySelector("#sign-in-btn")
+        sign_in_btn.style.color = "#ffffff"
+        const log_in_btn = document.querySelector("#log-in-btn")
+        log_in_btn.style.color = "#ffffff"
+        const leaderboard_btn = document.querySelector("#leaderboard-btn")
+        leaderboard_btn.style.color = "#ffffff"
+        const settings_btn = document.querySelector("#settings-btn")
+        settings_btn.style.color = "#ffffff"
+        const start_btn = document.querySelector("#start-btn")
+        start_btn.style.color = "#ffffff"
+        const search_field = document.querySelector("#search-field")
+        search_field.style.color = "#ffffff"
+        mode = 1
+    }
+    else
+    {
+        const lightNavbar = document.querySelector(".rgb-shadow-light")
+        lightNavbar.className = "navbar col-2 p-0 me-2 rounded rgb-shadow-dark"
+        const lightMain = document.querySelector(".rgb-shadow-light")
+        lightMain.className = "main col ms-2 rounded rgb-shadow-dark"
+        lightBody = document.querySelector("body")
+        lightBody.style.backgroundColor = "#000000";
+        const sign_in_btn = document.querySelector("#sign-in-btn")
+        sign_in_btn.style.color = "#000000"
+        const log_in_btn = document.querySelector("#log-in-btn")
+        log_in_btn.style.color = "#000000"
+        const leaderboard_btn = document.querySelector("#leaderboard-btn")
+        leaderboard_btn.style.color = "#000000"
+        const settings_btn = document.querySelector("#settings-btn")
+        settings_btn.style.color = "#000000"
+        const start_btn = document.querySelector("#start-btn")
+        start_btn.style.color = "#000000"
+        const search_field = document.querySelector("#search-field")
+        search_field.style.color = "#000000"
+        mode = 0
+    }
 })
