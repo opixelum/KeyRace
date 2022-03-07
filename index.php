ef7dd8370c7f8441c3be30721e1c552ba538ab78
Opixelum <12w0zvkgazd3@opayq.net>
2022-03-07 14:51:14 +0100

    Fixed height

--- index.php
+++ index.php
@@ -23,39 +23,39 @@
         <!-- Main -->
         <div id="main" class="main h-100 col ms-2 rounded rgb-shadow">
           <div id="welcome" class="container-fluid h-100 p-3">
-          <div class="row h-30">
-            <div class="col-3 h-100">
-              <img src="./src/images/ts-cars.png" class="img-fluid" alt="Top-left cars">
+            <div class="row h-25">
+              <div class="col-3 h-100">
+                <img src="./src/images/ts-cars.png" class="img-fluid" alt="Top-left cars">
+              </div>
+              <div class="col-6 h-100 d-flex justify-content-center align-items-end rgb-text">
+                <h1 class="fw-bold text-center" style="font-size:60px; letter-spacing: 0.5em">KeyRace</h1>
+              </div>
+              <div class="col-3 h-100 d-flex justify-content-end">
+                <img alt="Top-right resources" src="./src/images/te-resources.png" class="img-fluid" class="img-fluid">
+              </div>
             </div>
-            <div class="col-6 h-100 d-flex justify-content-center align-items-end rgb-text">
-              <h1 class="fw-bold text-center mb-5" style="font-size:60px; letter-spacing: 0.5em">KeyRace</h1>
-            </div>
-            <div class="col-3 h-100 d-flex justify-content-end">
-              <img alt="Top-right resources" src="./src/images/te-resources.png" class="img-fluid" class="img-fluid">
-            </div>
-          </div>
 
-          <div class="row h-40">
-            <div class="col-3 h-100">
+            <div class="row h-50">
+              <div class="col-3 h-100">
+              </div>
+              <div class="col-6 h-100 d-flex flex-column justify-content-center align-item-center rgb-text" style="color:white; word-spacing:1em">
+                <h3 class="w-100 fs-1 text-center"><span>Race</span> against fast typers</h3>
+                <h3 class="w-100 fs-1 text-center">Show off your <span>speed</span></h3>
+                <h3 class="w-100 fs-1 text-center"><span>Play</span> and earn now!</h3>
+              </div>
+              <div class="col-3 h-100"></div>
             </div>
-            <div class="col-6 h-100 d-flex flex-column justify-content-center align-item-center rgb-text" style="color:white; word-spacing:1em">
-              <h3 class="w-100 fs-1 text-center"><span>Race</span> against fast typers</h3>
-              <h3 class="w-100 fs-1 text-center">Show off your <span>speed</span></h3>
-              <h3 class="w-100 fs-1 text-center"><span>Play</span> and earn now!</h3>
-            </div>
-            <div class="col-3 h-100"></div>
-          </div>
 
-          <div class="row h-30">
-            <div class="col-3 h-100 d-flex align-items-end">
-              <img src="./src/images/bs-avatars.png" class="img-fluid" alt="Bottom-left avatars">
-            </div>
-            <div class="col-6 h-100 d-flex justify-content-center align-items-start">
-              <button id="start-btn" class="btn">Start</button>
-            </div>
-            <div class="col-3 h-100 d-flex justify-content-end align-items-end">
-              <img src="./src/images/be-cars.png" class="img-fluid" alt="Bottom-right cars">
-            </div>
+            <div class="row h-25">
+              <div class="col-3 h-100 d-flex align-items-end">
+                <img src="./src/images/bs-avatars.png" class="img-fluid" alt="Bottom-left avatars">
+              </div>
+              <div class="col-6 h-100 d-flex justify-content-center align-items-start">
+                <button id="start-btn" class="btn">Start</button>
+              </div>
+              <div class="col-3 h-100 d-flex justify-content-end align-items-end">
+                <img src="./src/images/be-cars.png" class="img-fluid" alt="Bottom-right cars">
+              </div>
           </div>
         </div>
       </div>
