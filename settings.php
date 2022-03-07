f5e7527e80733f31d9428064119905a09fca8eb2
Opixelum <12w0zvkgazd3@opayq.net>
2022-03-07 16:27:16 +0100

    Added head.php & included it

--- settings.php
+++ settings.php
@@ -1,16 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./src/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./src/css/style.css">
-    <title>KeyRace | Settings</title>
-  </head>
+  <?php
+      $title = "Settings | KeyRace";
+      include("./src/includes/head.php");
+  ?>
 
   <body class="dark-theme">
     <div class="container-fluid vh-100 p-3">
