01d793ff5680817cc7e60f12cb3c5d23d9f1e97e
Opixelum <12w0zvkgazd3@opayq.net>
2022-03-08 15:33:15 +0100

    Added fields verification

--- src/scripts/php/signup_check.php
+++ src/scripts/php/signup_check.php
@@ -1,3 +1,69 @@
 <?php 
+    // Store user credentials in variables
+    $username         = $_POST["username"];
+    $email            = $_POST["email"];
+    $password         = $_POST["password"];
+    $confirm_password = $_POST["confirm-password"];
 
+
+    // *************** F I E L D S   V E R I F I C A T I O N S ****************
+
+    // If an error is caught, redirect to signup page with the error message
+
+    // If at least one field is empty
+    if (empty($username) || empty($email) ||
+    empty($password) || empty($confirm_password))
+    {
+        header("location: signup.php?error=Please fill all fields.");
+        exit();
+    }
+
+    // If username is too long
+    if (strlen($username) > 45)
+    {
+        header("location: signup.php?error=Username must be less than 45 characters long.");
+        exit();
+    }
+
+    // If email is too long
+    if (strlen($email) > 255)
+    {
+        header("location: signup.php?error=Email must be less than 255 characters long.");
+        exit();
+    }
+
+    // If email has wrong format
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        header("location: signup.php?error=Email has wrong format.");
+        exit();
+    }
+
+    // Store booleans for each requirement
+    $uppercase = preg_match("@[A-Z]@", $password);
+    $lowercase = preg_match("@[a-z]@", $password);
+    $number    = preg_match("@[0-9]@", $password);
+    $symbols   = preg_match("@[\w]@" , $password);
+    $length    = strlen($password) > 8;
+
+    // If password doesn't meet requirements
+    if (!($uppercase && $lowercase && $number && $symbols && $length))
+    {
+        header("location: signup.php?error=Password doesn't meet requirements.");
+        exit();
+    }
+
+    // If confirm_password is different than password
+    if ($password != $confirm_password)
+    {
+        header("location: signup.php?error=Confirm password is different.");
+        exit();
+    }
+
+    // Connect to database
+    include("./db_connect.php");
+
+
+    header("location: ../../../login.php");
+    exit();
 ?>
