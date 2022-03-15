ee060d85e168320046fcde41ef2441ed5b8ad2c0
Saygel <micujeremy02@gmail.com>
2022-03-15 14:47:43 +0100

    center des labels

--- signup.php
+++ signup.php
@@ -12,25 +12,25 @@
           <?php include("src/includes/navbar.php");?>
         </header>
 
-        <main class="col ms-2 rounded rgb-shadow">
-          <form method="POST" action="./src/scripts/php/signup_check.php">
-            <label for="username">Username</label><br>
+        <main class="col ms-2 d-flex justify-content-center align-items-center rounded rgb-shadow">
+          <form class="d-flex justify-content-center flex-wrap w-25" method="POST" action="./src/scripts/php/signup_check.php">
+            <label class="w-100 text-center" for="username">Username</label><br>
             <input type="text" id="username-inpt" name="username"
             placeholder="JDoe"><br><br>
 
-            <label for="email">Email</label><br>
+            <label class="w-100 text-center" for="email">Email</label><br>
             <input type="email" id="email-inpt" name="email"
             placeholder="john.doe@email.com"><br><br>
 
-            <label for="password">Password</label><br>
+            <label class="w-100 text-center" for="password">Password</label><br>
             <input type="password" id="password-inpt" name="password"
             placeholder="••••••••••••••••"><br><br>
 
-            <label for="confirm-password">Confirm password</label><br>
+            <label class="w-100 text-center" for="confirm-password">Confirm password</label><br>
             <input type="password" id="confirm-password-inpt"
             name="confirm-password" placeholder="••••••••••••••••"><br><br>
 
-            <label for="keyboard-layout">Keyboard layout</label><br>
+            <label class="w-100 text-center" for="keyboard-layout">Keyboard layout</label><br>
             <select name="keyboard-layout" id="keyboard-layout-drpdwn">
               <option value="">--- Choose a layout ---</option>
               <option value="1">QWERTY</option>
@@ -38,7 +38,7 @@
               <option value="3">DVORAK</option>
             </select><br><br>
 
-            <input type="submit" placeholder="submit">
+            <input class="w-100" type="submit" placeholder="submit">
           </form>
 
           <?php
