0f1a4af2e61c6168b02f3bdda8fa0fe420cefc12
Romain <romain.nerot@outlook.fr>
2022-03-19 22:45:43 +0100

    Captcha logo

--- login.php
+++ login.php
@@ -29,14 +29,14 @@
           </form>
 
           <div id="puzzle_container">
-            <div class="puzzle_block">1</div>
-            <div class="puzzle_block">2</div>
-            <div class="puzzle_block">3</div>
-            <div class="puzzle_block">4</div>
-            <div class="puzzle_block">5</div>
-            <div class="puzzle_block">6</div>
-            <div class="puzzle_block">7</div>
-            <div class="puzzle_block">8</div>
+            <div class="puzzle_block"><img src="./src/images/captcha/captcha1.png"></div>
+            <div class="puzzle_block"><img src="./src/images/captcha/captcha2.png"></div>
+            <div class="puzzle_block"><img src="./src/images/captcha/captcha3.png"></div>
+            <div class="puzzle_block"><img src="./src/images/captcha/captcha4.png"></div>
+            <div class="puzzle_block"><img src="./src/images/captcha/captcha5.png"></div>
+            <div class="puzzle_block"><img src="./src/images/captcha/captcha6.png"></div>
+            <div class="puzzle_block"><img src="./src/images/captcha/captcha7.png"></div>
+            <div class="puzzle_block"><img src="./src/images/captcha/captcha8.png"></div>
           </div>
           <div id="reset_container">
             <div class="reset_button">Reset</div>
