a365edc6147d2366ec889b6d5f99080d9cc6c57b
Opixelum <12w0zvkgazd3@opayq.net>
2022-03-20 20:40:15 +0100

    Refactored signup.php

--- signup.php
+++ signup.php
@@ -15,34 +15,76 @@
         <main class="col ms-2 rounded rgb-shadow">
           <form method="POST" action="./src/scripts/php/signup_check.php">
             <label for="username">Username</label><br>
-            <input type="text" value="<?php echo isset($_COOKIE["username"]) ? $_COOKIE["username"] : '';?>" 
-            id="username-inpt" name="username" placeholder="JDoe" required><br><br>
+            <input
+              type="text"
+              value="<?php 
+                  echo isset($_COOKIE["username"]) ? $_COOKIE["username"] : '';
+              ?>" 
+              id="username-inpt"
+              name="username"
+              placeholder="JDoe"
+              required
+            ><br><br>
 
             <label for="email">Email</label><br>
-            <input type="email" value="<?php echo isset($_COOKIE["email"]) ? $_COOKIE["email"] : '';?>" 
-            id="email-inpt" name="email" placeholder="john.doe@email.com" required><br><br>
+            <input
+              type="email"
+              value="<?php
+                  echo isset($_COOKIE["email"]) ? $_COOKIE["email"] : '';
+              ?>" 
+              id="email-inpt"
+              name="email"
+              placeholder="john.doe@email.com"
+              required
+            ><br><br>
 
             <label for="password">Password</label><br>
-            <input type="password" id="password-inpt" name="password"
-            placeholder="••••••••••••••••" required><br><br>
+            <input
+              type="password"
+              id="password-inpt"
+              name="password"
+              placeholder="••••••••••••••••"
+              required
+            ><br><br>
 
             <label for="confirm-password">Confirm password</label><br>
-            <input type="password" id="confirm-password-inpt"
-            name="confirm-password" placeholder="••••••••••••••••" required><br><br>
+            <input
+              type="password"
+              id="confirm-password-inpt"
+              name="confirm-password"
+              placeholder="••••••••••••••••"
+              required
+            ><br><br>
 
             <label for="keyboard-layout">Keyboard layout</label><br>
-            <select name="keyboard-layout" id="keyboard-layout-drpdwn" required>
+            <select
+              name="keyboard-layout"
+              id="keyboard-layout-drpdwn"
+              required
+            >
               <option value=''>--- Choose a layout ---</option>
               <option value='1'
-                <?php echo isset($_COOKIE["keyboard_layout"]) && $_COOKIE["keyboard_layout"] == 1 ? "selected" : '' ?>
+                <?php // Select saved keyboard layout from cookie
+                    echo
+                        isset($_COOKIE["keyboard_layout"]) &&
+                        $_COOKIE["keyboard_layout"] == 1 ? "selected" : '';
+                ?>
               >QWERTY</option>
 
               <option value='2'
-                <?php echo isset($_COOKIE["keyboard_layout"]) && $_COOKIE["keyboard_layout"] == 2 ? "selected" : '' ?>
+                <?php // Select saved keyboard layout from cookie
+                    echo
+                        isset($_COOKIE["keyboard_layout"]) &&
+                        $_COOKIE["keyboard_layout"] == 2 ? "selected" : '';
+                ?>
               >AZERTY</option>
 
               <option value='3'
-                <?php echo isset($_COOKIE["keyboard_layout"]) && $_COOKIE["keyboard_layout"] == 3 ? "selected" : '' ?>
+                <?php // Select saved keyboard layout from cookie
+                    echo
+                        isset($_COOKIE["keyboard_layout"]) &&
+                        $_COOKIE["keyboard_layout"] == 3 ? "selected" : '';
+                ?>
               >DVORAK</option>
             </select><br><br>
 
