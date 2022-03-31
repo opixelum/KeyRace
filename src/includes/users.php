71da967edc881a78330a86935371270eb8b825ed
Romain <romain.nerot@outlook.fr>
2022-03-31 18:14:23 +0200

    css

--- src/includes/users.php
+++ src/includes/users.php
@@ -6,7 +6,7 @@
         $results = $req->fetchAll(PDO::FETCH_ASSOC);
     ?>
 
-    <table class="table table-striped table-bordered dark-theme">
+    <table class="table table-bordered">
         <tr>
             <th>Username</th>
             <th>Email</th>
@@ -19,7 +19,8 @@
                 echo '<td>' . $user['username'] . '</td>';
                 echo '<td>' . $user['email'] . '</td>';
                 echo '<td class="text-nowrap">';
-                echo '<a class="btn btn-primary btn-sm me-2 col-4" href="http://localhost/KeyRace/src/includes/read.php?id=' . $user['user_id'] . '">See</a>';
+                echo '<a class="btn btn-primary btn-sm me-2 col-4" 
+                href="http://localhost/KeyRace/src/includes/read.php?id=' . $user['user_id'] . '">See</a>';
                 echo '<a class="btn btn-success btn-sm me-2 col-4" href="update.php?id=' . $user['user_id'] . '">Modify</a>';
                 echo '<a class="btn btn-danger btn-sm me-2 col-4" href="delete.php?id=' . $user['user_id'] . '">Delete</a>';
                 echo '</td>';
