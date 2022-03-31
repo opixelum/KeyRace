16057f741626d5057290706d01234f6b258a150e
Opixelum <12w0zvkgazd3@opayq.net>
2022-03-31 20:26:28 +0200

    Fixed broken links

--- campaign/1.php
+++ campaign/1.php
@@ -2,14 +2,14 @@
 <html lang="en">
   <?php
       $title = "Home | KeyRace";
-      include("src/includes/head.php");
+      include("../src/includes/head.php");
   ?>
 
   <body class="dark-theme">
     <div class="container-fluid vh-100 g-0">
       <div class="row h-100 p-3 g-0">
         <header class="col-2 p-0 me-2 rounded rgb-shadow">
-          <?php include("src/includes/navbar.php"); ?>
+          <?php include("../src/includes/navbar.php"); ?>
         </header>
 
         <main class="col h-100 ms-2 rounded rgb-shadow">
@@ -19,6 +19,6 @@
       </div>
     </div>
 
-    <script src="src/scripts/js/main.js"></script>
+    <script src="../src/scripts/js/main.js"></script>
   </body>
 </html>
