132fb4c5461b84fb7ae1cd029b5c9189fbe8a418
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-01 21:16:20 +0200

    Fixed captcha block border's color

--- src/includes/captcha.php
+++ src/includes/captcha.php
@@ -1,13 +1,13 @@
 <div class="d-flex justify-content-evenly align-items-center h-100">
   <div class="h-100" id="captcha">
-    <div class="block"><img class="border w-100" src="./src/images/captcha/captcha1.png"></div>
-    <div class="block"><img class="border w-100" src="./src/images/captcha/captcha2.png"></div>
-    <div class="block"><img class="border w-100" src="./src/images/captcha/captcha3.png"></div>
-    <div class="block"><img class="border w-100" src="./src/images/captcha/captcha4.png"></div>
-    <div class="block"><img class="border w-100" src="./src/images/captcha/captcha5.png"></div>
-    <div class="block"><img class="border w-100" src="./src/images/captcha/captcha6.png"></div>
-    <div class="block"><img class="border w-100" src="./src/images/captcha/captcha7.png"></div>
-    <div class="block"><img class="border w-100" src="./src/images/captcha/captcha8.png"></div>
+    <div class="block"><img class="w-100" src="./src/images/captcha/captcha1.png"></div>
+    <div class="block"><img class="w-100" src="./src/images/captcha/captcha2.png"></div>
+    <div class="block"><img class="w-100" src="./src/images/captcha/captcha3.png"></div>
+    <div class="block"><img class="w-100" src="./src/images/captcha/captcha4.png"></div>
+    <div class="block"><img class="w-100" src="./src/images/captcha/captcha5.png"></div>
+    <div class="block"><img class="w-100" src="./src/images/captcha/captcha6.png"></div>
+    <div class="block"><img class="w-100" src="./src/images/captcha/captcha7.png"></div>
+    <div class="block"><img class="w-100" src="./src/images/captcha/captcha8.png"></div>
   </div>
     
   <div class="btn" id="reset-btn">Reset</div>
