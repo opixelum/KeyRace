f712d9520c1f01ee6c125091295b3a8fd3b672ed
Saygel <micujeremy02@gmail.com>
2022-04-01 19:20:32 +0200

    FIxed captcha's borders <3

--- src/includes/captcha.php
+++ src/includes/captcha.php
@@ -1,13 +1,13 @@
 <div class="d-flex align-items-center">
   <div id="captcha_container">
-    <div class="captcha_block"><img src="./src/images/captcha/captcha1.png"></div>
-    <div class="captcha_block"><img src="./src/images/captcha/captcha2.png"></div>
-    <div class="captcha_block"><img src="./src/images/captcha/captcha3.png"></div>
-    <div class="captcha_block"><img src="./src/images/captcha/captcha4.png"></div>
-    <div class="captcha_block"><img src="./src/images/captcha/captcha5.png"></div>
-    <div class="captcha_block"><img src="./src/images/captcha/captcha6.png"></div>
-    <div class="captcha_block"><img src="./src/images/captcha/captcha7.png"></div>
-    <div class="captcha_block"><img src="./src/images/captcha/captcha8.png"></div>
+    <div class="captcha_block"><img class="border" src="./src/images/captcha/captcha1.png"></div>
+    <div class="captcha_block"><img class="border" src="./src/images/captcha/captcha2.png"></div>
+    <div class="captcha_block"><img class="border" src="./src/images/captcha/captcha3.png"></div>
+    <div class="captcha_block"><img class="border" src="./src/images/captcha/captcha4.png"></div>
+    <div class="captcha_block"><img class="border" src="./src/images/captcha/captcha5.png"></div>
+    <div class="captcha_block"><img class="border" src="./src/images/captcha/captcha6.png"></div>
+    <div class="captcha_block"><img class="border" src="./src/images/captcha/captcha7.png"></div>
+    <div class="captcha_block"><img class="border" src="./src/images/captcha/captcha8.png"></div>
   </div>
     
   <button class="btn" id="reset-btn">Reset</button>
