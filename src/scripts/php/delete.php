c18bf8aff48315110f13691a2a7e8bc39f8e99fd
Romain <romain.nerot@outlook.fr>
2022-04-02 18:55:28 +0200

    delete an user

---
+++ src/scripts/php/delete.php
@@ -0,0 +1,8 @@
+<?php 
+    include('C:\MAMP\htdocs\KeyRace\src\scripts\php\db_connect.php');
+    $q = "DELETE FROM USER WHERE user_id = $_GET[id]";
+    $req = $db->query($q);
+    $results = $req->fetchAll(PDO::FETCH_ASSOC);
+
+    header("location:../../../settings.php");
+?>
