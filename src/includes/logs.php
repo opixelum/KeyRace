8d8f0e4f7af99903d79647e46f2cf86737b42c6e
Romain <romain.nerot@outlook.fr>
2022-04-04 10:12:10 +0200

    close file logs

--- src/includes/logs.php
+++ src/includes/logs.php
@@ -5,10 +5,12 @@
             $file = fopen("../../../logs/login_attempts.csv", "a");
             $line = $_POST["email"] . ',' . date("Y-m-d H:i:s") . ',' . $status . "\n";
             fwrite($file, $line);
+            fclose($file);
 
         case "page":
             $file = fopen("logs/page_visits.csv", "a");
             $line = (isset($_SESSION["email"]) ? $_SESSION["email"] : "guest") . ',' . date("Y-m-d H:i:s") . ',' . $page ."\n";
             fwrite($file, $line);
+            fclose($file);
     }
 ?>
