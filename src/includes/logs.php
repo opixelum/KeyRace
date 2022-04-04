88d4536f6aac181327b17465ce538f2e9e1085bb
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-04 18:45:13 +0200

    Fix logs switch statement

--- src/includes/logs.php
+++ src/includes/logs.php
@@ -6,11 +6,13 @@
             $line = $_POST["email"] . ',' . date("Y-m-d H:i:s") . ',' . $status . "\n";
             fwrite($file, $line);
             fclose($file);
+            break;
 
         case "page":
             $file = fopen("logs/page_visits.csv", "a");
             $line = (isset($_SESSION["email"]) ? $_SESSION["email"] : "guest") . ',' . date("Y-m-d H:i:s") . ',' . $page ."\n";
             fwrite($file, $line);
             fclose($file);
+            break;
     }
 ?>
