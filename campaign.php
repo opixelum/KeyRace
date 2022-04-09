967313fa5b16f33bbf9e828b7302f42e596b9457
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-10 00:34:35 +0200

    Feat: add quest check

--- campaign.php
+++ campaign.php
@@ -35,7 +35,7 @@ include("src/includes/head.php");
       <main class="col d-flex justify-content-center align-items-start flex-wrap h-100 ms-2 rounded rgb-shadow">
         <?php
         // If no quest parameter in url or user's max quest is lower, display campaign menu
-        if (!isset($_GET["quest"]) || $_GET["quest"] < 1 || $_GET["quest"] > 8 && $result[0]['quest'] < $_GET["quest"])
+        if (!isset($_GET["quest"]) || ($_GET["quest"] < 1 || $_GET["quest"] > 8) && $result[0]["quest"] < $_GET["quest"])
           include("src/includes/campaign_menu.php");
 
         // If quest parameter in url, display corresponding quest
