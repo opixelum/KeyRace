2accc7fc966f2e148a2cd0d7aa05d1d7971b5d1d
Romain <romain.nerot@outlook.fr>
2022-04-09 20:26:15 +0200

    customization + images

---
+++ customization.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php
+      $page = "settings";
+      $title = "Settings | KeyRace";
+      include("./src/includes/head.php");
+  ?>
+
+  <body class="dark-theme">
+    <div class="container-fluid vh-100 g-0">
+      <div class="row h-100 p-3 g-0">
+        <header class="col-2 p-0 me-2 rounded rgb-shadow">
+          <?php include("src/includes/navbar.php");?>
+        </header>
+
+        <main class="col ms-2 rounded rgb-shadow">
+
+        </main>
+      </div>
+    </div>
+
+    <script src="./src/scripts/js/avatar_maker.js"></script>
+    <script src="./src/scripts/js/main.js"></script>
+  </body>
+</html>
