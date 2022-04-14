b1815918a3e11c386b5dc9884d0cc2aa69b3f2ad
Romain <romain.nerot@outlook.fr>
2022-04-14 19:55:46 +0200

    Update customization.js

--- src/scripts/js/customization.js
+++ src/scripts/js/customization.js
@@ -1,13 +1,39 @@
+const customizationMenu = document.querySelector('#customization-menu')
+const carBtn = document.querySelector('#car-btn')
+if (carBtn) {
+    carBtn.addEventListener('click', () => {
+        customizationMenu.innerHTML = `
+        <h1>Customize your car</h1>
+        `
+    })
+}
+
 const avatarBtn = document.querySelector('#avatar-btn')
 if (avatarBtn) {
     avatarBtn.addEventListener('click', () => {
-        const avatarMenu = document.querySelector('#customization-menu')
-        avatarMenu.innerHTML = `
+        customizationMenu.innerHTML = `
         <button id="helmet-btn" class="btn col-2">Helmet</button>
         <button id="visor-btn" class="btn col-2">Visor</button>
         <button id="vest-btn" class="btn col-2">Vest</button>
         <button id="background-btn" class="btn col-2">Background</button>
         `
-        
+    })
+}
+
+const interfaceBtn = document.querySelector('#interface-btn')
+if (interfaceBtn) {
+    interfaceBtn.addEventListener('click', () => {
+        customizationMenu.innerHTML = `
+        <h1>Interface</h1>
+        `
+    })
+}
+
+const shopBtn = document.querySelector('#shop-btn')
+if (shopBtn) {
+    shopBtn.addEventListener('click', () => {
+        customizationMenu.innerHTML = `
+        <h1>Shop</h1>
+        `
     })
 }
