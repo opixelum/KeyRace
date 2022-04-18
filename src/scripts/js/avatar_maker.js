be49bbd2fb635577cb4ac4e205c715fcdf524b2a
Romain <romain.nerot@outlook.fr>
2022-04-18 17:39:10 +0200

    fix divs

--- src/scripts/js/avatar_maker.js
+++ src/scripts/js/avatar_maker.js
@@ -165,18 +165,18 @@ function displayAvatarMaker() {
     if (backgroundBtn) {
         backgroundBtn.addEventListener('click', () => {
             assetsSelection.innerHTML = `
-                <div id="classic-backgrounds" class="row">
+                <div class="w-100" id="classic-backgrounds" class="row">
                     <p class="w-100 m-0 bg-dark">Classic backgrounds</p>
                     <div class="bg-black w-25" id="black-div"><p></p></div>
                     <div class="bg-white w-25" id="white-div"><p></p></div>
                 </div>
-                <div id="primary-backgrounds" class="row">
+                <div class="w-100" id="primary-backgrounds" class="row">
                     <p class="w-100 m-0 bg-dark">Primary backgrounds</p>
                     <div class="w-25" id="red-div"><p></p></div>
                     <div class="w-25" id="green-div"><p></p></div>
                     <div class="w-25" id="blue-div"><p></p></div>
                 </div>
-                <div id="mix-backgrounds" class="row">
+                <div class="w-100" id="mix-backgrounds" class="row">
                     <p class="w-100 m-0 bg-dark">Mix backgrounds</p>
                     <div class="w-25" id="cyan-div"><p></p></div>
                     <div class="w-25" id="orange-div"><p></p></div>
