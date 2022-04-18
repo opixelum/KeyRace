87c48cf88ba780f35cfa6152d6c1ed37bc38521d
Romain <romain.nerot@outlook.fr>
2022-04-18 18:20:08 +0200

    update background

--- src/scripts/js/avatar_maker.js
+++ src/scripts/js/avatar_maker.js
@@ -167,22 +167,22 @@ function displayAvatarMaker() {
             assetsSelection.innerHTML = `
                 <div class="w-100" id="classic-backgrounds" class="row">
                     <p class="w-100 m-0 bg-dark">Classic backgrounds</p>
-                    <div class="bg-black w-25" id="black-div"><p></p></div>
-                    <div class="bg-white w-25" id="white-div"><p></p></div>
+                    <div class="m-0" id="black-div"><p></p></div>
+                    <div id="white-div"><p></p></div>
                 </div>
                 <div class="w-100" id="primary-backgrounds" class="row">
                     <p class="w-100 m-0 bg-dark">Primary backgrounds</p>
-                    <div class="w-25" id="red-div"><p></p></div>
-                    <div class="w-25" id="green-div"><p></p></div>
-                    <div class="w-25" id="blue-div"><p></p></div>
+                    <div id="red-div"><p></p></div>
+                    <div id="green-div"><p></p></div>
+                    <div id="blue-div"><p></p></div>
                 </div>
                 <div class="w-100" id="mix-backgrounds" class="row">
                     <p class="w-100 m-0 bg-dark">Mix backgrounds</p>
-                    <div class="w-25" id="cyan-div"><p></p></div>
-                    <div class="w-25" id="orange-div"><p></p></div>
-                    <div class="w-25" id="pink-div"><p></p></div>
-                    <div class="w-25" id="purple-div"><p></p></div>
-                    <div class="w-25" id="yellow-div"><p></p></div>                 
+                    <div id="cyan-div"><p></p></div>
+                    <div id="orange-div"><p></p></div>
+                    <div id="pink-div"><p></p></div>
+                    <div id="purple-div"><p></p></div>
+                    <div id="yellow-div"><p></p></div>                 
                 </div>
             `
         })
