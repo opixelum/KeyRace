8047b3924026022a22f9603e7bdc4deb8f36aa7f
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-20 19:31:18 +0200

    add stats

--- profile.php
+++ profile.php
@@ -25,9 +25,25 @@ if (!isset($_SESSION["id"]))
         </header>
 
         <main class="col h-100 ms-2 rounded rgb-shadow">
+          <!-- Profile assets -->
           <div class="d-flex flex-wrap justify-content-center h-25 w-100">
             <img alt="Banner" class="w-100 h-100" src="./src/images/banner.png">
             <img alt="Banner" class="profile-avatar rounded-circle" src="./src/images/avatar.png">
+          </div>
+          <!-- Stats -->
+          <div class="container my-4">
+            <div class="row">
+              <div class="col-5 d-flex justify-content-around">
+                <h5>Highest WPM: 106.5</h5>
+                <h5>Average WPM: 83</h5>
+              </div>
+              <div class="col-2">
+                  <!-- Spacing column -->
+              </div>
+              <div class="col-5 d-flex justify-content-around">
+                <h5>Rank: 2</h5>
+                <h5>Time played: 19h30min</h5>
+              </div>
           </div>
         </main>
       </div>
