6e399d39197de757c208bccc6a1bc8961855f73a
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-20 23:28:55 +0200

    center username & friends list button

--- profile.php
+++ profile.php
@@ -24,26 +24,36 @@ if (!isset($_SESSION["id"]))
           <?php include("src/includes/navbar.php"); ?>
         </header>
 
-        <main class="col h-100 ms-2 rounded rgb-shadow">
+        <main class="col container h-100 ms-2 rounded rgb-shadow">
           <!-- Profile assets -->
-          <div class="d-flex flex-wrap justify-content-center h-25 w-100">
-            <img alt="Banner" class="w-100 h-100" src="./src/images/banner.png">
-            <img alt="Banner" class="profile-avatar rounded-circle" src="./src/images/avatar.png">
+          <div class="row h-25">
+            <div class="col d-flex flex-wrap justify-content-center h-100 w-100">
+              <img alt="Banner" class="w-100 h-100" src="./src/images/banner.png">
+              <img alt="Banner" class="profile-avatar rounded-circle" src="./src/images/avatar.png">
+            </div>
           </div>
+
           <!-- Stats -->
-          <div class="container my-4">
-            <div class="row">
-              <div class="col-5 d-flex justify-content-around">
-                <h5>Highest WPM: 106.5</h5>
-                <h5>Average WPM: 83</h5>
-              </div>
-              <div class="col-2">
-                  <!-- Spacing column -->
-              </div>
-              <div class="col-5 d-flex justify-content-around">
-                <h5>Rank: 2</h5>
-                <h5>Time played: 19h30min</h5>
-              </div>
+          <div class="row my-4">
+            <div class="col-5 d-flex justify-content-around">
+              <h5>Highest WPM: 106.5</h5>
+              <h5>Average WPM: 83</h5>
+            </div>
+            <div class="col-2">
+              <!-- Spacing column -->
+            </div>
+            <div class="col-5 d-flex justify-content-around">
+              <h5>Rank: 2</h5>
+              <h5>Time played: 19h30min</h5>
+            </div>
+          </div>
+
+          <!-- Username & friends list button --> 
+          <div class="row">
+            <div class="col d-flex flex-wrap justify-content-center">
+              <h3 class="m-3 text-center w-100">Opixelum</h3>
+              <button class="btn">Friends list</button>
+            </div>
           </div>
         </main>
       </div>
