58ca2852fc91c1a5f5654ec598ba1199474fb96e
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-22 00:01:50 +0200

    Hotfix: update user_id name in queries

--- src/includes/users.php
+++ src/includes/users.php
@@ -1,7 +1,7 @@
 <div class="container">
     <?php 
         include('src/scripts/php/db_connect.php');
-        $q = 'SELECT user_id, username, email, role FROM USER';
+        $q = 'SELECT id, username, email, role FROM USER';
         $req = $db->query($q);
         $results = $req->fetchAll(PDO::FETCH_ASSOC);
     ?>
@@ -21,8 +21,8 @@
                 echo '<td>' . $user['email'] . '</td>';
                 echo '<td>' . $user['role'] . '</td>';
                 echo '<td class="text-nowrap">';
-                echo '<a class="btn btn-primary btn-sm me-2 col-6" href="read.php?id=' . $user['user_id'] . '">Edit</a>';
-                echo '<a id="delete-Btn" class="btn btn-danger btn-sm me-2 col-6" href="./src/scripts/php/delete.php?id=' . $user['user_id'] . '">Delete</a>';
+                echo '<a class="btn btn-primary btn-sm me-2 col-6" href="read.php?id=' . $user['id'] . '">Edit</a>';
+                echo '<a id="delete-Btn" class="btn btn-danger btn-sm me-2 col-6" href="./src/scripts/php/delete.php?id=' . $user['id'] . '">Delete</a>';
                 echo '</td>';
                 echo '</tr>';
             }
