95b4910307d9dabc0662df86999d9579c9892ea4
Romain <romain.nerot@outlook.fr>
2022-04-23 17:55:36 +0200

    design

--- training.php
+++ training.php
@@ -34,7 +34,9 @@
         </header>
 
         <main class="col d-flex flex-column justify-content-between align-items-center flex-wrap h-100 ms-2 rounded rgb-shadow p-5">
+            <h1 class="text-center w-100 mb-5">Training</h1>    
             <div id="race" class="border-start border-end w-75">
+                <hr>
                 <div class="w-100">
                     <img
                     alt="User car"
@@ -44,31 +46,31 @@
                     style="transform:translate(-99px)"
                     >
                 </div>
-                </div>
+                <hr>
+            </div>
 
-                <div class="container w-100">
+            <div class="container w-100">
                 <div class="row d-flex justify-content-between w-100">
                     <div class="col-3 ps-5">
-                    <p class="ms-5" id="time">Time: --- s</p>
+                        <p class="ms-5" id="time">Time: --- s</p>
                     </div>
                     <div class="col-3 ps-5">
-                    <p class="ms-5" id="wpm">WPM: ---</p>
+                        <p class="ms-5" id="wpm">WPM: ---</p>
                     </div>
                     <div class="col-3 ps-5">
-                    <p class="ms-5" id="accuracy">Accuracy: --- %</p>
+                        <p class="ms-5" id="accuracy">Accuracy: --- %</p>
                     </div>
                     <div class="col-3 ps-5">
-                    <p class="ms-5" id="errors">Errors: ---</p>
+                        <p class="ms-5" id="errors">Errors: ---</p>
                     </div>
-                </div>
-                <div class="fs-3 p-3 input-field rounded text-break" id="typing-field"></div>
                 </div>
             </div>
+            <div class="fs-3 p-3 input-field rounded text-break" id="typing-field"></div>
         </main>
       </div>
     </div>
 
     <script src="./src/scripts/js/main.js"></script>
-    <script src="./src/scripts/js/typing.js"></script>
+    <script src="./src/scripts/js/training.js"></script>
   </body>
 </html>
