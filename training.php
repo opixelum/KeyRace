4b347d16fb07163d8da7dbfabc58a305333f4981
Romain <romain.nerot@outlook.fr>
2022-04-23 17:16:42 +0200

    Update training.php

--- training.php
+++ training.php
@@ -34,7 +34,36 @@
         </header>
 
         <main class="col d-flex flex-column justify-content-between align-items-center flex-wrap h-100 ms-2 rounded rgb-shadow p-5">
-        
+            <div id="race" class="border-start border-end w-75">
+                <div class="w-100">
+                    <img
+                    alt="User car"
+                    id="user-car"
+                    src="src/images/cars/tank.png"
+                    width="100px"
+                    style="transform:translate(-99px)"
+                    >
+                </div>
+                </div>
+
+                <div class="container w-100">
+                <div class="row d-flex justify-content-between w-100">
+                    <div class="col-3 ps-5">
+                    <p class="ms-5" id="time">Time: --- s</p>
+                    </div>
+                    <div class="col-3 ps-5">
+                    <p class="ms-5" id="wpm">WPM: ---</p>
+                    </div>
+                    <div class="col-3 ps-5">
+                    <p class="ms-5" id="accuracy">Accuracy: --- %</p>
+                    </div>
+                    <div class="col-3 ps-5">
+                    <p class="ms-5" id="errors">Errors: ---</p>
+                    </div>
+                </div>
+                <div class="fs-3 p-3 input-field rounded text-break" id="typing-field"></div>
+                </div>
+            </div>
         </main>
       </div>
     </div>
