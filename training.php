1cfb745573640991583da20cf170fba41494cb57
Romain <romain.nerot@outlook.fr>
2022-04-23 17:05:54 +0200

    training

--- training.php
+++ training.php
@@ -21,8 +21,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-$page = "campaign";
-$title = "Campaign | KeyRace";
+$page = "training";
+$title = "Training | KeyRace";
 include("src/includes/head.php");
 ?>
 
@@ -34,22 +34,12 @@ include("src/includes/head.php");
       </header>
 
       <main class="col d-flex flex-column justify-content-between align-items-center flex-wrap h-100 ms-2 rounded rgb-shadow p-5">
-        <?php
-        // If no quest parameter in url or user's max quest is lower, display campaign menu
-        if (!isset($_GET["quest"]) || $_GET["quest"] < 1 || $_GET["quest"] > 8 || $result[0]["quest"] + 1 < $_GET["quest"])
-          include("src/includes/campaign_menu.php");
-
-        // If quest parameter in url, display corresponding quest
-        else {
-          $quest = $_GET["quest"];
-          include("src/includes/quest.php");
-        }
-        ?>
+        
       </main>
     </div>
   </div>
 
-  <script src="../src/scripts/js/main.js"></script>
+  <script src="./src/scripts/js/main.js"></script>
 </body>
 
 </html>
