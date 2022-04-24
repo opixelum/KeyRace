e2ca7c58a8057ac184935c1a15414e93f8fb4df9
Romain <romain.nerot@outlook.fr>
2022-04-24 19:02:47 +0200

    fix delete.php

--- src/scripts/php/delete.php
+++ src/scripts/php/delete.php
@@ -1,6 +1,6 @@
 <?php
     
-    include('C:\MAMP\htdocs\KeyRace\src\scripts\php\db_connect.php');
+    include('db_connect.php');
     $q = "DELETE FROM USER WHERE user_id = $_GET[id]";
     $req = $db->query($q);
     $results = $req->fetchAll(PDO::FETCH_ASSOC);
