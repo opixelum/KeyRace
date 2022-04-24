98c3cb4f53a7f7c97c30ea28e669a7f132f0b2f9
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-24 17:42:04 +0200

    Add vest & visor on canvas & add comments

--- src/scripts/js/avatar_maker.js
+++ src/scripts/js/avatar_maker.js
@@ -2,23 +2,26 @@ let helmet = new Image()
 let vest = new Image()
 let visor = new Image()
 
+// TO DO: Get current user's assets from database 
 helmet.src = './src/images/avatar/helmet/sport/helmet_sport_helmet_black.png'
-helmet.onload = function() {
-    buildAvatar()
-}
+vest.src = './src/images/avatar/vest/race/fur/body_race_fur_black.png'
+visor.src = `./src/images/avatar/visor/race/helmet_race_visor_white.png`
 
 function buildAvatar() {
     const avatar = document.querySelector("#avatarCanvas")
     const context = avatar.getContext("2d")
-    avatar.width = 300
-    avatar.height = 300
+
+    // Set image size
+    avatar.width = 500
+    avatar.height = 500
 
     // Draw rectangle as background
     context.beginPath()
-    context.rect(0, 0, 300, 300)
+    context.rect(0, 0, 500, 500)
     context.fillStyle = 'blue'
     context.fill()
 
+    // Place images on canvas
     context.drawImage(vest, 0, 0)
     context.drawImage(helmet, 0, 0)
     context.drawImage(visor, 0, 0)
