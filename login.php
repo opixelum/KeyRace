4fec01e0a757859f731b4aff2ee44ca287fe141f
Saygel <micujeremy02@gmail.com>
2022-04-24 15:07:37 +0200

    Update login.php

--- login.php
+++ login.php
@@ -23,49 +23,65 @@
           <?php include("src/includes/navbar.php");?>
         </header>
         
-        <main class="col ms-2 rounded rgb-shadow d-flex justify-content-center align-items-center">
-          <form class="d-flex justify-content-center flex-wrap" action="./src/scripts/php/login_check.php" method="POST">
-            <label class="w-100 text-center" for="email">Email</label><br>
-            <input
-              id="email-inpt"
-              name="email"
-              placeholder="john.doe@email.com"
-              required
-              type="email"
-              value="<?php
-                  // Set cookie value to input field
-                  echo isset($_COOKIE["email"]) ? $_COOKIE["email"] : '';
-              ?>" 
-            ><br><br>
+      <main class="col ms-2 rounded rgb-shadow d-flex justify-content-center align-items-center flex-wrap">
+        <form class="d-flex justify-content-center flex-wrap" action="./src/scripts/php/login_check.php" method="POST">
+        <h1>Log in</h1>
+        
+                  <label class="w-100 text-center" for="email">Email</label><br>
+                  <input
+                    class="w-50 h-25 input-field border-0 px-3 py-2 rounded"
+                    id="email-inpt"
+                    name="email"
+                    placeholder="john.doe@email.com"
+                    required
+                    type="email"
+                    value="<?php
+                        // Set cookie value to input field
+                        echo isset($_COOKIE["email"]) ? $_COOKIE["email"] : '';
+                    ?>" 
+                  ><br><br>
+       
 
-            <label class="w-100 text-center" for="password">Password</label><br>
-            <input
-              id="password-inpt"
-              name="password"
-              placeholder="••••••••••••••••"
-              required
-              type="password"
-            ><br><br>
+                <div class="container d-flex justify-content-center flex-wrap w-100 h-100">
+                    <label class="w-100 text-center" for="password">Password</label><br>
+                    <input
+                      class="w-50 h-25 input-field border-0 px-3 py-2 rounded"
+                      id="password-inpt"
+                      name="password"
+                      placeholder="••••••••••••••••"
+                      required
+                      type="password"
+                    ><br><br>
+                  </div>  
 
-            <input
-            id="stay-connected-chckbx"
-            name="stay-connected"
-            type="checkbox"
-            value="stay-connected"
-            >
-            <label for="stay-connected">Stay connected</label><br><br>
+                  <div class="container d-flex justify-content-center flex-wrap w-100 h-100">
 
-            <input placeholder="submit" type="submit">
-          </form>
+                    <input
+                        class="input-field"
+                        id="stay-connected-chckbx"
+                        name="stay-connected"
+                        type="checkbox"
+                        value="stay-connected"
+                        >
+                        <label for="stay-connected">Stay connected</label><br><br>
+                  </div>                        
 
+              <div class="d-flex flex-column justify-content-evenly"> 
+                <label class="w-100 text-center" for="captcha">Captcha</label>
+                <?php include("src/includes/captcha.php");?>
 
-          <div class="d-flex flex-column justify-content-evenly col"> 
-            <label class="w-100 text-center" for="captcha">Captcha</label>
-            <?php include("src/includes/captcha.php");?>
-                
-          </div>
+                                 
+              </div>
 
-          <?php include("src/includes/message.php");?>
+              <input class="btn" placeholder="submit" type="submit">
+
+              
+              
+                                            
+        </form>
+
+          <?php include("src/includes/message.php");?>                       
+
         </main>
       </div>
     </div>
