2812b057ec227ae90c507ffdc93845e9caf064ae
Saygel <micujeremy02@gmail.com>
2022-05-07 18:37:34 +0200

    Update forgotten_password.php

--- forgotten_password.php
+++ forgotten_password.php
@@ -1,9 +1,13 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <?php
+
+
+
       $page = "forgotten password";
       $title = "Forgotten password | KeyRace";
-      include('src/includes/head.php')
+      include('src/includes/head.php');
   ?>
 
 <?php
@@ -29,19 +33,80 @@ include("src\scripts\php\db_connect.php");
   </body>
 </html>
 
+
+
 <?php
+
+    use PHPMailer\PHPMailer\PHPMailer;  
+
+      require_once "src/includes/phpmailer/Exception.php"; 
+      require_once "src/includes/phpmailer/SMTP.php";
+      require_once "src/includes/phpmailer/PHPMailer.php";  
+
+     $mail = new PHPMailer(true);
+
+     $mail->isSMTP();
+     $mail->SMTPDebug = 1;
+     $mail->SMTPAuth = true;
+     $mail->SMTPSecure = 'ssl';
+     $mail->Host = "smtp.gmail.com";
+     $mail->Port = 465;
+     $mail->CharSet = "utf-8";
+
+     $mail->Username = "noreply.keyrace@gmail.com";
+     $mail->Password = "1|_aXutestrctrtgnfgbcmxuE";
+
+     $token = uniqid();
+     $url = "http://localhost/KeyRace/src/token?token=$token.php";
+ 
+     $message = "Bonjour, voici votre lien pour la réinitialisation du mot de passe : $url";
+     $message = wordwrap($message,70, "\r\n");
+     $headers = [
+       "From" => "keyrace@game.fr",
+       "Reply-To" => "adresse@site.fr",
+       "Cc" => "copie@site.fr",
+       "Bcc" => "copieCachée@site.fr",
+       "Content-Type" => "text/html; charset=utf-8"
+     ];
+     $to = $_POST['email'];
+     $subject =  'Mot de passe oublié';
+
+     $mail->setFrom("noreply.keyrace@gmail.com");
+     $mail->addAddress($to);
+     $mail->isHTML(true);
+     $mail->Subject = "Forgotten Password";
+
+     $mail->Body =
+     "
+         <a href='$url'>Click here</a> to change you password.
+     ";
+
+     $mail->AltBody =
+     "
+         Go to following link to change your password: $url.
+     ";
+ 
+     
+
+
+
+
+
+
+
+
+
+
+
+
 
 if(isset($_POST['email'])){
 
 
-    $token = uniqid();
-    $url = "http://localhost/KeyRace/src/token?token=$token.php";
-
-    $message = "Bonjour, voici votre lien pour la réinitialisation du mot de passe : $url";
-    $headers ='Content-Type: text/plain; charset="utf-8"'." ";
 
 
-    if(mail($_POST['email'], 'Mot de passe oublié', $message, $headers)){
+
+    if(mail($to,$subject, $message, $headers)){
 
         $sql = "UPDATE users SET token = ? WHERE email = ?";
         $statement = $db->prepare($sql);
