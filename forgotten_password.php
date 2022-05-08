95352503527d7047e282c65590f56583d30a2778
Saygel <micujeremy02@gmail.com>
2022-05-08 20:01:34 +0200

    Update forgotten_password.php

--- forgotten_password.php
+++ forgotten_password.php
@@ -3,7 +3,6 @@
 <html lang="en">
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
 
       $page = "forgotten password";
       $title = "Forgotten password | KeyRace";
@@ -12,7 +11,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 <?php
 
-include("src\scripts\php\db_connect.php");
+include("src/scripts/php/db_connect.php");
 
 
 ?>
@@ -23,7 +22,7 @@ include("src\scripts\php\db_connect.php");
   </head>
   <body>
     <h2>Forgot password</h2>
-    <form method="post">
+    <form action="src/scripts/php/forgotten_password_check.php" method="post">
       <div class="container">
         <label for="email"><b>Email</b></label>
         <input type="email" placeholder="Enter Email" name="email" required>
@@ -35,73 +34,4 @@ include("src\scripts\php\db_connect.php");
 
 
 
-<?php
 
-
-
-    require_once "src/includes/phpmailer/Exception.php"; 
-    require_once "src/includes/phpmailer/SMTP.php";
-    require_once "src/includes/phpmailer/PHPMailer.php";  
-    
-    $mail = new PHPMailer();
-
-
-
-
-      $mail->SMTPDebug = 1;
-
-      $mail->isSMTP();
-
-      $mail->Host = "smtp.gmail.com";
-
-      $mail->SMTPAuth = true;
-
-      $mail->SMTPSecure = 'ssl';
-
-      $mail->Host = "smtp.gmail.com";
-
-      $mail->Port = 465;
-
-
-      $mail->Username = "noreply.keyrace@gmail.com";
-      $mail->Password = "1|_aXutestrctrtgnfgbcmxuE";
-
-      $mail->isHTML(true);
-      $mail->Subject = "Confirmation email for KeyRace";
-      $url = "https://keyrace.online";
-
-
-      $mail->setFrom("noreply.keyrace@gmail.com");
-      $mail->addAddress("saygel93400@gmail.com");
-
-      $mail->Body =
-      "
-          <a href='$url'>Click here</a> to confirm your email.
-      ";
-      // Plain text body for non-HTML client
-      $mail->AltBody =
-      "
-          Go to following link to confirm your email: $url.
-      ";
-  
-
-      if(!$mail->send())
-      {
-          $message = "Confirmation email couldn't be sent. Please try again later.";
-
-          echo "Erreur";
-          exit;
-
-      }
-  
-      // If email has been sent
-      $message = "Almost done! Confirm your email before logging in ";
-      $message .= "(if you don't see it in your inbox, check your spams).";
-      
-      echo "Email envoyé";
-
-
-
-
-
-?>
