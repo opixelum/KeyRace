b90994552a9a368bf7fcef5aa14aca935665f8f3
Romain <romain.nerot@outlook.fr>
2022-05-11 18:30:30 +0200

    Update export.php

--- src/scripts/php/export.php
+++ src/scripts/php/export.php
@@ -1,14 +1,24 @@
 <?php
+session_start();
 
-require('../../includes/fpdf184/fpdf.php');
+// Connect to db
+include('src/scripts/php/db_connect.php');
 
+// Get all from informations from user
+$q = 'SELECT * FROM USER WHERE id = ' . $_SESSION['user_id'];
+$req = $db->query($q);
+$results = $req->fetchAll(PDO::FETCH_ASSOC);
 
-// $text ="Pseudo : " . $_GET["pseudo"] . "Prenom : " . $_GET["name"]
+require('..../includes/fpdf184/fpdf.php');
 
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,$text);
-$pdf->Output();
+$pdf->Cell(100,10,"My KeyRace account",0,1);
+$pdf->SetFont('Arial','N',12);
+$pdf->Cell(100,10,"Username : " . $results["username"],0,1);
+$pdf->Cell(100,10,"Email : " . $results["email"],0,1);
+$pdf->Cell(100,10,"keyboard : " . $results["keyboard"],0,1);
 
+$pdf->Output();
 ?>
