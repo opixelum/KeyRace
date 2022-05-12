2181f09bd0123c17254c7c1fa784e0b631fdb95b
Opixelum <12w0zvkgazd3@opayq.net>
2022-05-12 18:58:14 +0200

    Restore users.php

--- src/includes/users.php
+++ src/includes/users.php
@@ -1,7 +1,33 @@
 <?php
+    include('../scripts/php/db_connect.php');
+    $q = 'SELECT id, username, email, role FROM USER';
+    $req = $db->query($q);
+    $results = $req->fetchAll(PDO::FETCH_ASSOC);
+
     echo '
-          <div class="col w-100 d-flex flex-row justify-content-evenly align-items-center p-3">
+          <div class="col d-flex flex-row justify-content-evenly align-items-center p-3">
             <button id="account-btn" class="btn w-25">Account</button>
             <button id="database-btn" class="btn w-25">Database</button>
-          </div>'
+          </div>
+          <input type="text" id="search-input" placeholder="search">
+          <table class="table table-bordered overflow-auto h-75">
+            <tr>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Actions</th>
+            </tr>
+        ';
+        foreach ($results as $key => $user) {
+            echo '<tr>';
+            echo '<td>' . $user['username'] . '</td>';
+            echo '<td>' . $user['email'] . '</td>';
+            echo '<td>' . $user['role'] . '</td>';
+            echo '<td class="text-nowrap">';
+            echo '<a class="btn btn-primary btn-sm me-2 col-6" href="edit.php?id=' . $user['id'] . '">Edit</a>';
+            echo '<a id="delete-Btn" class="btn btn-danger btn-sm me-2 col-6" href="./src/scripts/php/delete.php?id=' . $user['id'] . '">Delete</a>';
+            echo '</td>';
+            echo '</tr>';
+        }
+    echo '</table>';
 ?>
