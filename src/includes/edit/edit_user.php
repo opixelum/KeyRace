ce690ff31fd3fc203d689d8be36d19932b7d2684
Romain <romain.nerot@outlook.fr>
2022-05-13 00:41:00 +0200

    add get

--- src/includes/edit/edit_user.php
+++ src/includes/edit/edit_user.php
@@ -7,7 +7,7 @@ $results = $req->fetchAll(PDO::FETCH_ASSOC);
 <div class="d-flex flex-column justify-content-center align-items-center">
 <h1>User</h1>
 </div>
-<form method="post" action="src/scripts/php/update.php?id='<?php echo $results[0]['id'] ?>'">
+<form method="post" action="src/scripts/php/update.php?id='<?php echo $_GET['id'] ?>'">
 <table class="table table-bordered">
     <?php
     echo '<tr>
