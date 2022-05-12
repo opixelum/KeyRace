36876d2477f37d3d5086702238de1f677db9057d
Romain <romain.nerot@outlook.fr>
2022-05-13 00:32:15 +0200

    Add 3 pages for stats, users, assets

---
+++ src/includes/edit/edit_assets.php
@@ -0,0 +1,50 @@
+<?php
+    include('src/scripts/php/db_connect.php');
+    $q = "SELECT helmet, visor, vest, background, car, banner FROM ASSETS WHERE user_id = $_GET[id]";
+    $req = $db->query($q);
+    $results = $req->fetchAll(PDO::FETCH_ASSOC);
+?>
+<div class="d-flex flex-column justify-content-center align-items-center">
+    <h1>Assets</h1>
+</div>
+<form method="post" action="src/scripts/php/update.php?id='<?php echo $results[0]['id'] ?>'">
+<table class="table table-bordered">
+    <?php
+    echo '<tr>
+            <td class="text-center">Helmet</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="helmet" type="text" value="' . $results[0]['helmet'] . '">
+            </td>
+            </tr>';
+    echo '<tr>
+            <td class="text-center">Visor</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="visor" type="text" value="' . $results[0]['visor'] . '">
+            </td>
+            </tr>';
+    echo '<tr>
+            <td class="text-center">Vest</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="vest" type="text" value="' . $results[0]['vest'] . '">
+            </td>
+            </tr>';
+    echo '<tr>
+            <td class="text-center">Background</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="background" type="text" value="' . $results[0]['background'] . '">
+            </td>
+            </tr>';
+    echo '<tr>
+            <td class="text-center">Car</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="car" type="text" value="' . $results[0]['car'] . '">
+            </td>
+            </tr>';
+    echo '<tr>
+            <td class="text-center">Banner</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="banner" type="text" value="' . $results[0]['banner'] . '">
+            </td>
+            </tr>';
+    ?>
+</table>
---
+++ src/includes/edit/edit_stats.php
@@ -0,0 +1,50 @@
+<?php
+    include('src/scripts/php/db_connect.php');
+    $q = "SELECT highest_wpm, races_ran, races_won, quest, achievements, time_played FROM STATS WHERE user_id = $_GET[id]";
+    $req = $db->query($q);
+    $results = $req->fetchAll(PDO::FETCH_ASSOC);
+?> 
+<div class="d-flex flex-column justify-content-center align-items-center">
+    <h1>Stats</h1>
+</div>
+<form method="post" action="src/scripts/php/update.php?id='<?php echo $results[0]['id'] ?>'">
+<table class="table table-bordered">
+    <?php
+    echo '<tr>
+            <td class="text-center">Highest_wpm</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="highest_wpm" type="number" value="' . $results[0]['highest_wpm'] . '">
+            </td>
+            </tr>';
+    echo '<tr>
+            <td class="text-center">Races_ran</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="races_ran" type="number" value="' . $results[0]['races_ran'] . '">
+            </td>
+            </tr>';
+    echo '<tr>
+            <td class="text-center">Races_won</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="races_won" type="number" value="' . $results[0]['races_won'] . '">
+            </td>
+            </tr>';
+    echo '<tr>
+            <td class="text-center">Quest</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="quest" type="number" value="' . $results[0]['quest'] . '">
+            </td>
+            </tr>';
+    echo '<tr>
+            <td class="text-center">Achievements</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="achievements" type="number" value="' . $results[0]['achievements'] . '">
+            </td>
+            </tr>';
+    echo '<tr>
+            <td class="text-center">Time_played</td>
+            <td class="text-center">
+                <input class="input-field border-0 px-3 py-2 rounded" name="time_played" type="number" value="' . $results[0]['time_played'] . '">
+            </td>
+            </tr>';
+    ?>
+</table>
