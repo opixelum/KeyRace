c80d62722309206793e005365b834600a91e17fa
Opixelum <12w0zvkgazd3@opayq.net>
2022-05-13 03:32:20 +0200

    Add images

--- src/scripts/js/car_display.js
+++ src/scripts/js/car_display.js
@@ -37,10 +37,12 @@ const displayCarSelection = () => {
     container.innerHTML = `
       <div class="row align-items-center h-100">
         <div class="col-6 d-flex flex-wrap align-items-start h-100 ps-5">
-          <div id="car-selection" class="d-flex flex-wrap border rounded-3 align-items-start h-75"></div>
+          <div id="car-selection" class="d-flex flex-wrap flex-column justify-content-evenly align-items-center border rounded-3 align-items-start h-75"></div>
         </div>
         <div class="col-6 d-flex flex-wrap justify-content-center align-items-center h-100">
-          <img alt="Selected car" class="w-100 p-5" id="selected-car">
+          <div class="d-flex align-items-center h-50">
+            <img alt="Selected car" class="w-100 p-5" id="selected-car">
+          </div>
           <button class="btn w-25 bg-danger mx-2" id="cancel-btn">Cancel</button>
           <button class="btn w-25 bg-success mx-2" id="save-btn">Save</button>
         </div>
@@ -51,23 +53,35 @@ const displayCarSelection = () => {
   const carSelection = document.querySelector("#car-selection")
   if (carSelection) {
     carSelection.innerHTML = `
-      <label class="w-25 p-2">
+      <label class="d-flex justify-content-center align-items-center w-25 p-2 h-25">
         <input type="radio" name="car" value="benz">
         <img src="src/images/cars/benz.png" class="w-100">
       </label>
-      <label class="w-25 p-2">
+      <label class="d-flex justify-content-center align-items-center w-25 p-2 h-25">
         <input type="radio" name="car" value="f1">
         <img src="src/images/cars/f1.png" class="w-100">
       </label>
-      <label class="w-25 p-2">
+      <label class="d-flex justify-content-center align-items-center w-25 p-2 h-25">
+        <input type="radio" name="car" value="henessi">
+        <img src="src/images/cars/henessi.png" class="w-100">
+      </label>
+      <label class="d-flex justify-content-center align-items-center w-25 p-2 h-25">
+        <input type="radio" name="car" value="jet">
+        <img src="src/images/cars/jet.png" class="w-100">
+      </label>
+      <label class="d-flex justify-content-center align-items-center w-25 p-2 h-25">
         <input type="radio" name="car" value="lambo">
         <img src="src/images/cars/lambo.png" class="w-100">
       </label>
-      <label class="w-25 p-2">
+      <label class="d-flex justify-content-center align-items-center w-25 p-2 h-25">
         <input type="radio" name="car" value="monospace">
         <img src="src/images/cars/monospace.png" class="w-100">
       </label>
-      <label class="w-25 p-2">
+      <label class="d-flex justify-content-center align-items-center w-25 p-2 h-25">
+        <input type="radio" name="car" value="pickup">
+        <img src="src/images/cars/pickup.png" class="w-100">
+      </label>
+      <label class="d-flex justify-content-center align-items-center w-25 p-2 h-25">
         <input type="radio" name="car" value="tank">
         <img src="src/images/cars/tank.png" class="w-100">
       </label>
